1a12903df55eb2a4404df3ee1a7bde6b413120d0
Oscar Martin Mora <moramartin99@gmail.com>
2022-05-27 15:24:56 -0600

    Arreglar la función repeatString en repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (mystring, number) {
+    let result = "";
+    for (let i = 1; i <= number; i++) {
+        result += mystring;
+    }
+    if (number < 0) {
+        return "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
