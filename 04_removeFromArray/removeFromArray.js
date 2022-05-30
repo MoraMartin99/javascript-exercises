4b7888380352d9e43551abf833b88f8d49ce39b5
Oscar Martin Mora <moramartin99@gmail.com>
2022-05-30 16:18:27 -0600

    Arreglar la función removeFromArray de removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeList) {
+    let result;
+    function checkValue(item) {
+        return removeList.some((arrayItem) => {
+            return item === arrayItem;
+        });
+    }
+    return (result = array.reduce((total, current) => {
+        if (!checkValue(current)) {
+            total.push(current);
+        }
+        return total;
+    }, []));
 };
 
 // Do not edit below this line
