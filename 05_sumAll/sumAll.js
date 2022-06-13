ec9f252056ea26b857e28b66d2ca61d0f18b2418
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-13 15:21:01 -0600

    Arreglar la función sumAll  en sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let argsAreInt = Number.isInteger(num1) && Number.isInteger(num2);
+    if (argsAreInt) {
+        if (num1 < 0 || num2 < 0) {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let result = 0;
 
+    for (min; min <= max; min++) {
+        result += min;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
