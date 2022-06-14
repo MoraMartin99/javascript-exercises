6e82cc40b52a70a4c70a770ee4a33b0c9caf881f
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-14 17:10:16 -0600

    Arreglar la función leapYears de leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let isDivisibleBy = (dividend, divisor) => {
+        return dividend % divisor == 0;
+    };
+    if (isDivisibleBy(year, 4)) {
+        if (!isDivisibleBy(year, 100)) {
+            return true;
+        } else if (isDivisibleBy(year, 400)) {
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
