684c96212abdbf9de2bdabcda5da543b2b34d217
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-16 15:14:22 -0600

    Arreglar las funciones en calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (myArr) {
+    return myArr.reduce((total, actual) => {
+        return total + actual;
+    }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (myArr) {
+    return myArr.reduce((total, actual) => {
+        total *= actual;
+        return total;
+    });
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    let result = 1;
+    for (let i = 1; i <= b; i++) {
+        result *= a;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+    let result = 1;
+    for (number; number >= 1; number--) {
+        result *= number;
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
