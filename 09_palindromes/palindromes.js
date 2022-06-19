b3d83707718d12e67a7c4f6b7c4a1c7e043568d8
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-19 14:20:18 -0600

    Arreglar función palindromes en palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let regex = /[\w]/gi;
+    let arr = word.match(regex);
+    return arr.join("").toLowerCase() === arr.reverse().join("").toLowerCase()
+        ? true
+        : false;
 };
 
 // Do not edit below this line
