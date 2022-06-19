6491c67a66e47811e6268cc2e0241194e9348c7e
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-19 16:03:28 -0600

    Arreglar función fibonacci en fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+    member = typeof member == "number" ? member : parseInt(member);
+    if (member > 0) {
+        let result = [];
+        for (i = 1; i <= member; i++) {
+            if (i == 1 || i == 2) {
+                result.push(1);
+            } else {
+                result.push(result[i - 3] + result[i - 2]);
+            }
+        }
+        return result[member - 1];
+    }
+    return "OOPS";
 };
 
 // Do not edit below this line
