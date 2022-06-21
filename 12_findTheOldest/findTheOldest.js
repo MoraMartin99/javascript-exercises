db452518746cb8660caf722eb935dc4c3178562d
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-21 14:56:38 -0600

    Arreglar función findTheOldest en findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let index = 0;
+    let age = 0;
+    people.forEach((element, currentIndex) => {
+        let currentAge;
+        if (Object.hasOwn(element, "yearOfDeath")) {
+            currentAge = element.yearOfDeath - element.yearOfBirth;
+        } else {
+            let time = new Date();
+            currentAge = time.getFullYear() - element.yearOfBirth;
+        }
+        if (currentAge >= age) {
+            age = currentAge;
+            index = currentIndex;
+        }
+    });
+    return people[index];
 };
 
 // Do not edit below this line
