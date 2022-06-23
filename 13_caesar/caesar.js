c325506e6a41ac3613c5bb1115ec795877a5d44c
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-22 18:41:56 -0600

    Arreglar función caesar en caesar.js

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,56 @@
-const caesar = function() {
+const caesar = function (string, offset) {
+    let alphabet = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ];
+    return string.split("").reduce((total, currentLetter) => {
+        let isABC = /[a-z]/i.test(currentLetter);
+        if (isABC) {
+            let alphabetIndex =
+                alphabet.findIndex((item) => {
+                    return item == currentLetter.toLowerCase();
+                }) + offset;
 
+            while (alphabetIndex > alphabet.length - 1) {
+                alphabetIndex -= alphabet.length;
+            }
+            while (alphabetIndex < (alphabet.length - 1) * -1) {
+                alphabetIndex += alphabet.length;
+            }
+            let changeLetter =
+                currentLetter == currentLetter.toUpperCase()
+                    ? alphabet.slice(alphabetIndex)[0].toUpperCase()
+                    : alphabet.slice(alphabetIndex)[0];
+            total = total.concat(changeLetter);
+        } else {
+            total = total.concat(currentLetter);
+        }
+        return total;
+    }, "");
 };
 
 // Do not edit below this line
