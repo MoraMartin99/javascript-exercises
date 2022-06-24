45d32496f23ce6a2123fa8881f4761915f501329
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-24 15:00:02 -0600

    Arreglar función pigLatin en pigLatin.js

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,37 @@
 function pigLatin(string) {
+    let arrRegEx = /[a-z]+/gi;
+    let arr = string.match(arrRegEx);
+    return arr.reduce((total, current, index) => {
+        let startVowel = /^[aeiou]/gi.test(current);
+        let startConsonant = /^[^aeiou\d\W]/gi.test(current);
+        if (startVowel) {
+            total.push(current.concat("ay"));
+        }
+        if (startConsonant) {
+            let startSpecialCase = /(^[^aeiou\d\W]+qu|^qu)([a-z]*)/gi.test(
+                current
+            );
+            if (startSpecialCase) {
+                total.push(
+                    current.replace(
+                        /(^[^aeiou\d\W]+qu|^qu)([a-z]*)/gi,
+                        "$2$1ay"
+                    )
+                );
+            } else {
+                total.push(
+                    current.replace(/(^[^aeiou\d\W]+)([a-z]*)/gi, "$2$1ay")
+                );
+            }
+        }
 
-};
-  
+        if (index == arr.length - 1) {
+            return total.join(" ");
+        } else {
+            return total;
+        }
+    }, []);
+}
+
 // Do not edit below this line
 module.exports = pigLatin;
