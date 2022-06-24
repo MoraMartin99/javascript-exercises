e4ead96e2799b592faa600c8daad1ba01c1a5d28
Oscar Martin Mora <moramartin99@gmail.com>
2022-06-24 16:40:07 -0600

    Arreglar función snakeCase de snakeCase.js

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,12 @@
-const snakeCase = function() {
-
+const snakeCase = function (string) {
+    let regex =
+        /[a-zA-Z]+(?=\.)|(?<=\.)[a-zA-Z]+|[a-z]+|[A-Z][a-z]+|[a-z]+[A-Z][a-z]+/g;
+    return string
+        .match(regex)
+        .map((item) => {
+            return item.toLowerCase();
+        })
+        .join("_");
 };
 
 // Do not edit below this line
